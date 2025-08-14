5fbb127fa7fa0829fca30cdc4d35d1e455683215
Sajhir98 <allanuc1428@gmail.com>
2025-08-14 12:48:01 -0500

    Repaso del Modulo 5
    Reconstruyendo el repositorio para estar al dia con las tareas, se hizo un repaso de Distintas funciones del gameloop de Unity

--- ProyectoEnsayoEBAC/Assets/Scripts/HolaMundo.cs
+++ ProyectoEnsayoEBAC/Assets/Scripts/HolaMundo.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+
+    int x;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        x = 0;
         print("Algo Paso");
         Debug.Log("Algo paso");
         Debug.LogWarning("AlgoSalioMedianamenteMal");
@@ -15,6 +18,28 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //x = x + 1;
+        //Debug.Log(x);
+
+        Debug.Log("Hola Desde Update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log("Hola desde Fixed Update cada 50 frames");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde Late Update");
+    }
+
+    private void OnEnable()
+    {
+        Debug.LogWarning("El Objeto ha sido Habilitado");
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogWarning("El objeto ha sido inhabilitado");
     }
 }
